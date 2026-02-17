f188d165aad5e221d6349a0b4813955ac2e32d61
Nieves Alonso Gilsanz <nievalon@ucm.es>
2026-02-17 11:54:32 +0100

    Update Separacion.cs

--- Assets/Scripts/Comportamientos/Separacion.cs
+++ Assets/Scripts/Comportamientos/Separacion.cs
@@ -11,6 +11,7 @@
 using System.Collections;
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 namespace UCM.IAV.Movimiento
@@ -37,8 +38,25 @@ namespace UCM.IAV.Movimiento
 
         public override ComportamientoDireccion GetComportamientoDireccion()
         {
-            // IMPLEMENTAR separación
-            return new ComportamientoDireccion();
+            ComportamientoDireccion result = new ComportamientoDireccion();
+
+            foreach(var target in targets)
+            {
+                UnityEngine.Vector3 direction = target.transform.position - agente.transform.position;
+
+                float distance = direction.magnitude;
+
+                if (distance < umbral)
+                {
+                    // Calcula la fuerza de la repulsion -> INVERSE SQUARE LAW
+                    float strength = Mathf.Min(decayCoefficient / (Mathf.Pow(distance, 2)), agente.aceleracionMax);
+
+                    direction.Normalize();
+                    result.lineal += strength * direction;
+                }
+            }
+
+            return result;
         }
     }
 }
