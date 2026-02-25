78ef584285ec670411da7fc8d166784f4a9ead62
Cynthia Tristán <cyntrist@ucm.es>
2026-02-25 12:40:48 +0100

    merodear idle refactorizado
    ahora tienen idle de verdad

--- Assets/Scripts/Comportamientos/Merodear.cs
+++ Assets/Scripts/Comportamientos/Merodear.cs
@@ -29,21 +29,24 @@ namespace UCM.IAV.Movimiento
         [SerializeField]
         private float wanderRadius = 5.0f; // radius of wander circle
 
-        [SerializeField]
-        float tiempoMaximo = 2; // maximum rate of wanderers orientation change
-
-        [SerializeField]
-        float tiempoMinimo = 1; // maximum rate of wanderers orientation change
-
-        float currentTimeObjective = 0.0f;
-        float limitTimeObjective = 0.0f;
-
-        float currentTimeIdle = 0.0f;
-        float limitTimeIdle = 0.0f;
-
         private float wanderOrientation = 0.0f;
 
         [SerializeField] private bool debugMerodeo = false;
+
+        [SerializeField]
+        float tiempoMaximo = 2.0f; // maximum rate of wanderers orientation change
+        [SerializeField]
+        float tiempoMinimo = 1.0f; // maximum rate of wanderers orientation change
+
+        //float currentTimeObjective = 0.0f;
+        //float limitTimeObjective = 0.0f;
+
+        //float currentTimeIdle = 0.0f;
+        //float limitTimeIdle = 0.0f;
+
+        private bool idle = false;
+        private float timer = 0.0f;
+        private float duration = 0.0f;
 
         private void Start()
         {
@@ -51,70 +54,70 @@ namespace UCM.IAV.Movimiento
             {
                 objetivo = new GameObject();
             }
-            limitTimeIdle = Random.Range(tiempoMinimo, tiempoMaximo);
-            currentTimeIdle = limitTimeIdle;
+            duration = Random.Range(tiempoMinimo, tiempoMaximo);
         }
 
         public override ComportamientoDireccion GetComportamientoDireccion()
         {
             ComportamientoDireccion result = new ComportamientoDireccion();
 
-            currentTimeIdle += Time.deltaTime;
-            if (currentTimeIdle < limitTimeIdle)
-            {
+            timer += Time.deltaTime;
+
+            if (idle)
+            { // si estamos en estado de idle
+                if (timer >= duration) // si se ha de acabar el idle
+                {
+                    // --- 1: NUEVO OBJETIVO
+                    // actualiza la direccion de merodeo
+                    wanderOrientation = Random.Range(.0f, 360f);
+
+                    // calcula el centro del wander circle
+                    objetivo.transform.position = agente.transform.position + wanderOffset * agente.OriToVec(wanderOrientation);
+
+                    // calcula targetOrientation
+                    float targetOrientation = wanderOrientation + agente.orientacion;
+
+                    // posicion del target
+                    objetivo.transform.position += wanderRadius * agente.OriToVec(targetOrientation);
+
+                    // salir del idle
+                    idle = false;
+                    timer = 0f;
+                }
+                // si estamos en idle no hacemos nada
                 return result;
             }
 
-            currentTimeObjective += Time.deltaTime;
+            // --- 2: CÁLCULO HASTA EL OBJETIVO
+            // direccion hacia el objetivo
+            Vector3 direccion = objetivo.transform.position - agente.transform.position;
 
-            if (currentTimeObjective >= limitTimeObjective)
+            // distancia hacia el objetivo
+            float distancia = direccion.magnitude;
+
+            if (timer >= duration || distancia < wanderRadius) // si ha pasado suficiente tiempo o ha llegado al radio objetivo
             {
-                limitTimeIdle = Random.Range(tiempoMinimo, tiempoMaximo);
-                currentTimeIdle = 0.0f;
+                idle = true;
+                timer = 0f;
+                duration = Random.Range(tiempoMinimo, tiempoMaximo);
 
-                currentTimeObjective = 0;
-                limitTimeObjective = Random.Range(tiempoMinimo, tiempoMaximo);
+                return result;
+            }
 
-                // --- 1
-                // actualiza la direccion de merodeo
-                wanderOrientation = Random.Range(.0f, 360f);
+            direccion.Normalize();
 
-                // calcula el centro del wander circle
-                objetivo.transform.position = agente.transform.position + wanderOffset * agente.OriToVec(wanderOrientation);
+            result.angular = 0;
 
-                // calcula targetOrientation
-                float targetOrientation = wanderOrientation + agente.orientacion;
+            // --- 3
+            // lineal = max aceleracion en la direccion de la orientacion
+            result.lineal = agente.aceleracionMax * direccion;
 
-                // posicion del target
-                objetivo.transform.position += wanderRadius * agente.OriToVec(targetOrientation);
-
-                // --- 2
-                // direccion hacia el objetivo
-                Vector3 direccion = objetivo.transform.position - agente.transform.position;
-
-                // distancia hacia el objetivo
-                float distancia = direccion.magnitude;
-
-                // hemos llegado -> paramos
-                if (distancia < wanderRadius)
-                {
-                    return result; // intencion de detenerse
-                }
-
-                result.angular = 0;
-
-                // --- 3
-                // lineal = max aceleracion en la direccion de la orientacion
-                result.lineal = agente.aceleracionMax * direccion;
-
-                if (debugMerodeo)
-                {
-                    Debug.DrawLine(agente.transform.position, objetivo.transform.position, new Color(0, 0, 1), 0.2f);
-                }
+            if (debugMerodeo)
+            {
+                Debug.DrawLine(agente.transform.position, objetivo.transform.position, new Color(0, 0, 1), 0.2f);
             }
 
             return result;
         }
-
     }
 }
